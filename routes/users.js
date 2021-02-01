05e4e320353918d39fea324f9855b1d55621c769
walt <walter.hills5@gmail.com>
2021-02-01 15:55:20 -0600

    Added utils.js, auth.js, worked on login routes for users all as a group

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,61 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const db = require("../models");
+const bcrypt = require("bcryptjs");
+const { loginUser, logoutUser, restoreUser, requireAuth } = require("../auth");
+const {
+  loginValidators,
+  userValidators,
+  validationResult,
+  csrfProtection,
+  asyncHandler,
+} = require("./utils");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.render("login");
 });
+//log user in
+router.post(
+  "/login",
+  csrfProtection,
+  loginValidators,
+  asyncHandler(async (req, res, next) => {
+    const errors = [];
+    const validatorErrors = validationResult(req);
+    const { email, password } = req.body;
+    const user = await db.User.findOne({ where: { email } });
+    if (validatorErrors.isEmpty()) {
+      if (user) {
+        const passwordMatch = await bcrypt.compare(
+          password,
+          user.hashedPassword.toString()
+        );
+        if (passwordMatch) {
+          loginUser(req, res, user);
+          return req.session.save(() => {
+            return res.redirect("/home");
+          });
+        } else {
+          errors.push("Login failed - Invalid Credentials");
+        }
+      } else {
+        errors.push("Login failed - Invalid Credentials");
+        errors = validatorErrors.array().map((error) => error.msg);
+      }
+    } else
+      res.render("/", {
+        title: "Login",
+        token: req.csrfToken(),
+        errors,
+      });
+  })
+);
+//register / signup
+router.post(
+  "/",
+  csrfProteciton,
+  asyncHandler(async (req, res, next) => {})
+);
 
 module.exports = router;
---
+++ routes/utils.js
@@ -0,0 +1,68 @@
+const csrf = require("csurf");
+const csrfProtection = csrf({ cookie: true });
+const { check, validationResult } = require("express-validator");
+const asyncHandler = (handler) => (req, res, next) =>
+  handler(req, res, next).catch(next);
+
+const userValidators = [
+  check("first_name")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a First Name")
+    .isLength({ max: 20 })
+    .withMessage("First Name must be less than 20 characters in length"),
+  check("last_name")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a Last Name")
+    .isLength({ max: 20 })
+    .withMessage("Last Name must be less than 20 characters in length"),
+  check("email")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide an Email")
+    .isLength({ max: 255 })
+    .withMessage("Email must be less than 255 characters in length")
+    .isEmail()
+    .withMessage("Email is invalid")
+    .custom((value) => {
+      return db.User.findOne({ where: { email: value } }).then((user) => {
+        if (user) {
+          return Promise.reject("The provided Email is already in use");
+        }
+      });
+    }),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a Password")
+    .isLength({ max: 50 })
+    .withMessage("Password must be less than 50 characters in length")
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, "g")
+    .withMessage(
+      'Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'
+    ),
+  check("confirmPassword")
+    .exists({ checkFalsy: true })
+    .withMessage("Please confirm your Password")
+    .isLength({ max: 50 })
+    .withMessage("Password must be less than 50 characters in length")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords do not match");
+      }
+      return true;
+    }),
+];
+const loginValidators = [
+  check("email")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide an Email"),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a Password"),
+];
+module.exports = {
+  csrfProtection,
+  asyncHandler,
+  loginValidators,
+  userValidators,
+  validationResult,
+  check,
+};
