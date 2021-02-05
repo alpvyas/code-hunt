20133ba28146464193ae81acd372b202cbf707b2
Yassine Cherradi <yassine.cherradi@yahoo.com>
2021-02-05 18:58:02 +0100

    worked on profile page with JM delete functionality

--- public/javascripts/profile-delete.js
+++ public/javascripts/profile-delete.js
@@ -1,11 +1,16 @@
 
 
-const deleteButton = document.querySelector('.delete-logo');
+const deleteButtons = document.querySelectorAll('.delete-logo');
+const mylinks = document.querySelector(".links-container")
 
+deleteButtons.forEach((button) => {
 
-deleteButton.addEventListener('click', async (event) => {
-  const postId = parseInt(event.target.id, 10);
-  const link = await fetch(`/posts/${postId}/delete`, { method: "DELETE" })
-  link = await link.json();  
-  // return link;    
+  button.addEventListener('click', async (event) => {
+    const postId = parseInt(event.target.id, 10);
+    const link = await fetch(`/posts/${postId}/delete`, { method: "DELETE" })
+    const linkdiv = document.getElementById(`mylinks${event.target.id}`);
+    mylinks.removeChild(linkdiv);
+    // link = await link.json();  
+    // return link;    
+  })
 })
