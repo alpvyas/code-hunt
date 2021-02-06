22b03b7bf4f404d775ba9510648eb3f0bc17da9d
walt <walter.hills5@gmail.com>
2021-02-06 13:40:13 -0600

    almost finished styling

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,41 +1,41 @@
-const registerTab = document.querySelector(".register-tab");
-const loginTab = document.querySelector(".login-tab");
+const registerTab = document.querySelector(".regBtn");
+const loginTab = document.querySelector(".logBtn");
 
 const registerForm = document.getElementById("register");
 const loginForm = document.getElementById("login");
 
 const errorDisplay = document.querySelector(".error-display");
 const demoBtn = document.querySelector(".demoBtn");
+window.addEventListener("DOMContentLoaded", (e) => {
+  registerTab.addEventListener("click", () => {
+    if (loginForm.classList === "hidden") {
+      loginForm.classList.remove("hidden");
+      registerForm.classList.add("hidden");
+      errorDisplay.classList.remove("hidden");
+      loginTab.classList.add("active");
+      registerTab.classList.remove("active");
+    } else {
+      loginForm.classList.add("hidden");
+      registerForm.classList.remove("hidden");
+      errorDisplay.classList.add("hidden");
+      loginTab.classList.remove("active");
+      registerTab.classList.add("active");
+    }
+  });
 
-registerTab.addEventListener("click", () => {
-  if (loginForm.classList === "hidden") {
-    
-    loginForm.classList.remove("hidden");
-    registerForm.classList.add("hidden");
-    errorDisplay.classList.remove("hidden");
-    loginTab.classList.add("active");
-    registerTab.classList.remove("active");
-  } else {
-    loginForm.classList.add("hidden");
-    registerForm.classList.remove("hidden");
-    errorDisplay.classList.add("hidden");
-    loginTab.classList.remove("active");
-    registerTab.classList.add("active");
-  }
-});
-
-loginTab.addEventListener("click", () => {
-  if (registerForm.classList === "hidden") {
-    registerForm.classList.remove("hidden");
-    loginForm.classList.add("hidden");
-    errorDisplay.classList.remove("hidden");
-    loginTab.classList.remove("active");
-    registerTab.classList.add("active");
-  } else {
-    registerForm.classList.add("hidden");
-    loginForm.classList.remove("hidden");
-    errorDisplay.classList.add("hidden");
-    loginTab.classList.add("active");
-    registerTab.classList.remove("active");
-  }
+  loginTab.addEventListener("click", () => {
+    if (registerForm.classList === "hidden") {
+      registerForm.classList.remove("hidden");
+      loginForm.classList.add("hidden");
+      errorDisplay.classList.remove("hidden");
+      loginTab.classList.remove("active");
+      registerTab.classList.add("active");
+    } else {
+      registerForm.classList.add("hidden");
+      loginForm.classList.remove("hidden");
+      errorDisplay.classList.add("hidden");
+      loginTab.classList.add("active");
+      registerTab.classList.remove("active");
+    }
+  });
 });
